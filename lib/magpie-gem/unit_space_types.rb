6727b6432fb82164639973e05490cc14715ceabf
Zai Thottakath <zaithottakath@gmail.com>
2015-06-01 13:35:43 -0700

    Creation of use types extracted into method and then used to generically generate json

--- lib/magpie-gem/unit_space_types.rb
+++ lib/magpie-gem/unit_space_types.rb
@@ -2,16 +2,30 @@ require 'magpie-gem/unit_space_type.rb'
 
 module Magpie
   class UnitSpaceTypes < Magpie::Base
-    has_one :office, :class => Magpie::UnitSpaceType, :context => 'unit'
-    has_one :retail, :class => Magpie::UnitSpaceType, :context => 'unit'
-    has_one :industrial, :class => Magpie::UnitSpaceType, :context => 'unit'
-    attr_accessor :office, :retail, :industrial
 
-    def initialize
-      @industrial = Magpie::UnitSpaceType.new
-      @office = Magpie::UnitSpaceType.new
-      @retail = Magpie::UnitSpaceType.new
+    class << self
+      def expose_types(*types)
+        raise "Types must be Symbols" unless types.all?{ |type| type.is_a? Symbol }
+        types.each do |type|
+          has_one type, class: Magpie::UnitSpaceType, context: 'unit'
+          attr_writer(type)
+          define_method(type) do
+            variable_name = "@#{ type }"
+            value = instance_variable_get(variable_name)
+            return value if value
+            instance_variable_set(variable_name, Magpie::UnitSpaceType.new)
+          end
+        end
+
+        use_types.merge types
+      end
+
+      def use_types
+        @types ||= Set.new
+      end
     end
+
+    expose_types :office, :industrial, :retail
 
     def use_types
       unless @use_types
@@ -27,30 +41,30 @@ module Magpie
       other_rsf = space.available_rsf - office_rsf
 
       if use_types.length > 0 && use_types[0] == "Industrial"
-        @industrial.load_from_model(space)
-        @industrial.available = other_rsf
-        @industrial.rate = Magpie::Rate.new(space.warehouse_rate, space.min_asking_rate, space.max_asking_rate)
+        industrial.load_from_model(space)
+        industrial.available = other_rsf
+        industrial.rate = Magpie::Rate.new(space.warehouse_rate, space.min_asking_rate, space.max_asking_rate)
 
         if office_rsf > 0
-          @office.load_from_model(space)
-          @office.available = space.office_rsf
-          @office.rate = Magpie::Rate.new(space.office_rate, space.min_asking_rate, space.max_asking_rate)
+          office.load_from_model(space)
+          office.available = space.office_rsf
+          office.rate = Magpie::Rate.new(space.office_rate, space.min_asking_rate, space.max_asking_rate)
         end
       elsif use_types.length > 0 && use_types[0] == "Retail"
-        @retail.load_from_model(space)
-        @retail.available = other_rsf
-        @retail.rate = Magpie::Rate.new(nil, space.min_asking_rate, space.max_asking_rate)
+        retail.load_from_model(space)
+        retail.available = other_rsf
+        retail.rate = Magpie::Rate.new(nil, space.min_asking_rate, space.max_asking_rate)
 
         if office_rsf > 0
-          @office.load_from_model(space)
-          @office.available = space.office_rsf
-          @office.rate = Magpie::Rate.new(space.office_rate, space.min_asking_rate, space.max_asking_rate)
+          office.load_from_model(space)
+          office.available = space.office_rsf
+          office.rate = Magpie::Rate.new(space.office_rate, space.min_asking_rate, space.max_asking_rate)
         end
       else
         # only office
-        @office.load_from_model(space)
-        @office.available = space.available_rsf
-        @office.rate = Magpie::Rate.new(nil, space.min_asking_rate, space.max_asking_rate)
+        office.load_from_model(space)
+        office.available = space.available_rsf
+        office.rate = Magpie::Rate.new(nil, space.min_asking_rate, space.max_asking_rate)
       end
 
       self
@@ -58,18 +72,18 @@ module Magpie
 
     def model_attributes_base
       {
-        office_rsf: @office.try(:available),
-        office_rate: @office.try(:rate).try(:value),
-        warehouse_rate: @industrial.try(:rate).try(:value)
+        office_rsf: office.try(:available),
+        office_rate: office.try(:rate).try(:value),
+        warehouse_rate: industrial.try(:rate).try(:value)
       }
     end
 
     def as_json(options={})
-      {
-        office: @office.as_json,
-        retail: @retail.as_json,
-        industrial: @industrial.as_json
-      }.reject{|k,v| v.nil? || v.empty?}
+      Hash[
+        self.class.use_types.map do |type|
+          [type, public_send(type).as_json]
+        end
+      ].reject{|k,v| v.nil? || v.empty?}
     end
   end
 end
